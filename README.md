87602877642f93e960ff327499c1649c6553f6e3
ac-scruffyk <acarva1@yahoo.com>
2024-04-27 15:38:46 -0500

    add new click logic, remove animated gradient

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # CRT Magnet Interference
 
-A cubic Bezier curve experiment that ended up simulating a kid, a magnet, and an old TV. Move the mouse, click to freeze.
+A cubic Bezier curve experiment that ended up simulating a kid, a magnet, and an old TV. Move the mouse, click to change color palette.
--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let magnetX = innerWidth / 2, magnetY = innerHeight / 2, 
-    r = Math.floor(255 * Math.random()), rDir = Math.ceil(Math.random() * 5), 
-    g = Math.floor(255 * Math.random()), gDir = Math.ceil(Math.random() * 5), 
-    b = Math.floor(255 * Math.random()), bDir = Math.ceil(Math.random() * 5), 
+    r = Math.floor(255 * Math.random()), 
+    g = Math.floor(255 * Math.random()), 
+    b = Math.floor(255 * Math.random()), 
     controlScale = 0;		//control point distance from the mouse position
-    frozen = 0, 				//0 or 1, determining if the grid is locked
     gridSize = 10;
 
 let canvas = document.getElementById('canvas');
@@ -21,7 +20,7 @@ window.onresize = function(){
 
 document.addEventListener('mousemove', moveMagnet);
 document.addEventListener('touchmove', moveMagnet, {passive: false});
-document.addEventListener('mousedown', toggleMagnetLock);
+document.addEventListener('click', refreshColors);
 
 function moveMagnet(e) {
   if (e.changedTouches) {
@@ -29,55 +28,49 @@ function moveMagnet(e) {
     e = e.touches[0];
   }
 
-  if (!frozen) {
-    magnetX = e.touches ? e.touches[0].clientX : e.clientX;
-    magnetY = e.touches ? e.touches[0].clientY : e.clientY;
-  }
+  magnetX = e.touches ? e.touches[0].clientX : e.clientX;
+  if (magnetX < 0) { magnetX = 0; }
+  if (magnetX > canvas.width) { magnetX = canvas.width }
+  magnetY = e.touches ? e.touches[0].clientY : e.clientY;
+  if (magnetY < 0) { magnetY = 0; }
+  if (magnetY > canvas.height) { magnetY = canvas.height }
 }
 
-function toggleMagnetLock() {
-  if (frozen) {
-    frozen = 0;
-  } else {
-    frozen = 1;
-  }
+function refreshColors() {
+  let newGrad = ctx.createLinearGradient(0, 0, innerWidth, innerHeight);
+
+  r = Math.floor(255 * Math.random());
+  g = Math.floor(255 * Math.random());
+  b = Math.floor(255 * Math.random());
+
+  newGrad.addColorStop(0, 'rgb(' + r + ',255,255)');
+  newGrad.addColorStop(0.33, 'rgb(255,' + g + ',255)');
+  newGrad.addColorStop(0.66, 'rgb(255,255,' + b + ')');
+  newGrad.addColorStop(1, 'rgb(' + r + ',' + g + ',' + b + ')');
+
+  gridGrad = newGrad;
 }
 
 let currentTime = Date.now();
 
+let gridGrad = ctx.createLinearGradient(0, 0, innerWidth, innerHeight); 
+
+gridGrad.addColorStop(0, 'rgb(' + r + ',255,255)');
+gridGrad.addColorStop(0.33, 'rgb(255,' + g + ',255)');
+gridGrad.addColorStop(0.66, 'rgb(255,255,' + b + ')');
+gridGrad.addColorStop(1, 'rgb(' + r + ',' + g + ',' + b + ')');
+
 function draw(){
   let frameTime = Date.now();
-
+  
   if (frameTime - currentTime < 16) {
     window.requestAnimationFrame(draw);
     return;
   }
-
+  
   frameTime = currentTime;
-
+  
   ctx.clearRect(0,0,innerWidth,innerHeight);
-  
-  let gridGrad = ctx.createLinearGradient(0, 0, innerWidth, innerHeight);
-  gridGrad.addColorStop(0, 'rgb(' + r + ',255,255)');
-  gridGrad.addColorStop(0.33, 'rgb(255,' + g + ',255)');
-  gridGrad.addColorStop(0.66, 'rgb(255,255,' + b + ')');
-  gridGrad.addColorStop(1, 'rgb(' + r + ',' + g + ',' + b + ')');
-  
-  if (r + rDir <= 255 && r + rDir >= 0) {
-    r += rDir;
-  } else {
-    rDir *= -1;
-  }
-  if (g + gDir <= 255 && g + gDir >= 0) {
-    g += gDir;
-  } else {
-    gDir *= -1;
-  }
-  if (b + bDir <= 255 && b + bDir >= 0) {
-    b += bDir;
-  } else {
-    bDir *= -1;
-  }
   
   ctx.strokeStyle = gridGrad;
   for (let i = 0 - innerWidth * 3; i < innerWidth * 4; i += gridSize) {
